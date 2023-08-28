890feef1f6b4355139c94304a5d8a49a3cdc3758
bthnaslnn <91845955+bthnaslnn@users.noreply.github.com>
2023-08-28 10:59:25 +0300

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Net.Http.Headers;
+using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using DataAccess.Concrete.InMemory;
+
+namespace ConsoleUI
+{
+
+    //SOLID 
+    //Open Closed Principle 
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ProductManager productManager = new ProductManager(new EfProductDal());
+
+            foreach (var product in productManager.GetByUnitPrice(40,100))
+            {
+                Console.WriteLine(product.ProductName);
+            }
+
+            
+        }
+    }
+}
+  
---
+++ DataAccess/Abstract/IProductDal.cs
@@ -0,0 +1,17 @@
+﻿using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Abstract
+{
+
+    //interface metotları default olarak publicdir.
+
+    public interface IProductDal :IEntityRepository<Product>
+    {
+        
+    }
+}
